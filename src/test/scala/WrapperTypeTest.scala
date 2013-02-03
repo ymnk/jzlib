99d63349a70b85b3ef9bac65ea18bd79d4f6ae33
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-02-03 13:52:43 -0800

    inflater should be able to detect the format of deflated data.
      * an enum type JZlib.WrapperType is added.
          JZlib.W_NONE
          JZlib.W_ZLIB
          JZlib.W_GZIP
          JZlib.W_ANY
    
      * Inflater with JZlib.W_ANY can inflate any deflated data
        even if it has zlib header or gzip header or no-header.
    
      * Deflate can easily generate zlib-ed data, gzip-ed data or nowrap data
        with a WrapperyType parameter.
    
      * following methods are added,
        ZStream#inflateInit(JZlib.WrapperType)
        ZStream#inflateInit(int, JZlib.WrapperType)
        ZStream#deflateInit(int, int, int, JZlib.WrapperType)
    
      * refer following test for how-to specify Wrappertype,
        src/test/scala/WrapperTypeTest.scala

---
+++ src/test/scala/WrapperTypeTest.scala
@@ -0,0 +1,147 @@
+/* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
+package com.jcraft.jzlib
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.{Test, Before}
+import org.junit.Assert._
+import org.hamcrest.CoreMatchers._
+import java.io.{ByteArrayOutputStream => BAOS, ByteArrayInputStream => BAIS}
+
+import JZlib._
+
+@RunWith(classOf[JUnit4])
+class WrapperTypeTest {
+  val data = "hello, hello!".getBytes
+
+  val comprLen = 40000
+  val uncomprLen = comprLen
+  var compr:Array[Byte] = _
+  var uncompr:Array[Byte] = _
+  var err: Int = _
+
+  @Before
+  def setUp = {
+    compr = new Array[Byte](comprLen)
+    uncompr = new Array[Byte](uncomprLen)
+
+    err = Z_OK
+  }
+
+  @Test
+  def w_zlib = {
+    val deflater = new ZStream
+
+    err = deflater.deflateInit(Z_BEST_SPEED, JZlib.DEF_WBITS, 9, JZlib.W_ZLIB)
+    assertThat(err, is(Z_OK))
+
+    deflate(deflater, data, compr)
+
+    List(JZlib.W_ZLIB, JZlib.W_ANY) foreach { w =>
+      val inflater = inflate(compr, uncompr, w)
+      val total_out = inflater.total_out.asInstanceOf[Int]
+      assertThat(new String(uncompr, 0, total_out), is(new String(data)))
+    }
+
+    List(JZlib.W_GZIP, JZlib.W_NONE) foreach { w =>
+      inflate_fail(compr, uncompr, w)
+    }
+  }
+
+  @Test
+  def w_none = {
+    val deflater = new ZStream
+
+    err = deflater.deflateInit(Z_BEST_SPEED, JZlib.DEF_WBITS, 9, JZlib.W_NONE)
+    assertThat(err, is(Z_OK))
+
+    deflate(deflater, data, compr)
+
+    List(JZlib.W_NONE, JZlib.W_ANY) foreach { w =>
+      val inflater = inflate(compr, uncompr, w)
+      val total_out = inflater.total_out.asInstanceOf[Int]
+      assertThat(new String(uncompr, 0, total_out), is(new String(data)))
+    }
+
+    List(JZlib.W_GZIP, JZlib.W_ZLIB) foreach { w =>
+      inflate_fail(compr, uncompr, w)
+    }
+  }
+
+  @Test
+  def w_gzip = {
+
+    val deflater = new ZStream
+
+    err = deflater.deflateInit(Z_BEST_SPEED, JZlib.DEF_WBITS, 9, JZlib.W_GZIP)
+    assertThat(err, is(Z_OK))
+
+    deflate(deflater, data, compr)
+
+    List(JZlib.W_GZIP, JZlib.W_ANY) foreach { w =>
+      val inflater = inflate(compr, uncompr, w)
+      val total_out = inflater.total_out.asInstanceOf[Int]
+      assertThat(new String(uncompr, 0, total_out), is(new String(data)))
+    }
+
+    List(JZlib.W_ZLIB, JZlib.W_NONE) foreach { w =>
+      inflate_fail(compr, uncompr, w)
+    }
+  }
+
+  private def deflate(deflater: ZStream,
+                      data: Array[Byte], compr: Array[Byte]) = {
+    deflater.setInput(data)
+    deflater.setOutput(compr)
+
+    err = deflater.deflate(JZlib.Z_FINISH)
+    assertThat(err, is(Z_STREAM_END))
+
+    err = deflater.end
+    assertThat(err, is(Z_OK))
+  }    
+
+  private def inflate(compr: Array[Byte],
+                      uncompr: Array[Byte],
+                      w: WrapperType) = {
+    val inflater = new ZStream
+    err = inflater.inflateInit(w)
+    assertThat(err, is(Z_OK))
+
+    inflater.setInput(compr)
+
+    var loop = true
+    while(loop) {
+      inflater.setOutput(uncompr)
+      err = inflater.inflate(Z_NO_FLUSH)
+      if(err == Z_STREAM_END) loop = false
+      else assertThat(err, is(Z_OK))
+    }
+    err = inflater.end
+    assertThat(err, is(Z_OK))
+
+    inflater
+  }
+
+  private def inflate_fail(compr: Array[Byte],
+                           uncompr: Array[Byte],
+                           w: WrapperType) = {
+    val inflater = new ZStream
+
+    err = inflater.inflateInit(w)
+    assertThat(err, is(Z_OK))
+
+    inflater.setInput(compr)
+
+    var loop = true
+    while(loop) {
+      inflater.setOutput(uncompr)
+      err = inflater.inflate(Z_NO_FLUSH)
+      if(err == Z_STREAM_END) loop = false
+      else {
+        assertThat(err, is(Z_DATA_ERROR))
+        loop = false
+      }
+    }
+  }
+}
