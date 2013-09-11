8b205d660d37503f71d4b1bbbc9779925ed14e8d
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2013-09-11 15:16:44 -0700

    ArrayIndexOutOfBoundsException in Tree.d_code   * https://github.com/ymnk/jzlib/issues/9   * fixed a bug in calculating the size of the pending buffer.   * added a test case based on https://github.com/jglick/jzlib-9-demo

--- src/main/java/com/jcraft/jzlib/Deflate.java
+++ src/main/java/com/jcraft/jzlib/Deflate.java
@@ -1384,7 +1384,7 @@ final class Deflate implements Cloneable {
     pending_buf = new byte[lit_bufsize*4];
     pending_buf_size = lit_bufsize*4;
 
-    d_buf = lit_bufsize/2;
+    d_buf = lit_bufsize;
     l_buf = (1+2)*lit_bufsize;
 
     this.level = level;
